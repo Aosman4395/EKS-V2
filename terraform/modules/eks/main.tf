38e98c6df2175d79da88c251d1b5364ebf330019
Aosman4395 <ahamed.osman4395@gmail.com>
2026-06-15 19:45:24 +0100

    updated eks

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -4,10 +4,10 @@ resource "aws_eks_cluster" "eks_cluster" {
   version  = var.kubernetes_version
   role_arn = aws_iam_role.eks_cluster_role.arn
 
-   access_config {
-    authentication_mode = "API_AND_CONFIG_MAP"
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
   }
-
 
   vpc_config {
     subnet_ids              = var.private_subnet_ids
